b2dd5acb0fc9b43da348e0f98bca3288e08c2bb4
dingff <dingff0923@foxmail.com>
2023-04-20 21:07:36 +0800

    fix(headers): serverIPAddress and response headers may be empty

--- src/pages/panel/components/Headers/index.tsx
+++ src/pages/panel/components/Headers/index.tsx
@@ -34,26 +34,30 @@ export default function Headers({ detail }: IProps) {
               <b>Status Code:</b>
               {detail.response.status}
             </div>
-            <div className={styles.listItem}>
-              <b>Remote Address:</b>
-              {detail.serverIPAddress}
-            </div>
+            {detail.serverIPAddress && (
+              <div className={styles.listItem}>
+                <b>Remote Address:</b>
+                {detail.serverIPAddress}
+              </div>
+            )}
             <div className={styles.listItem}>
               <b>Referrer Policy:</b>
               {getValueByName(detail.response.headers)('referrer-policy')}
             </div>
           </div>
         </Panel>
-        <Panel header="Response Headers" key="2">
-          <div className={styles.list}>
-            {detail.response.headers.map((item: any) => (
-              <div className={styles.listItem}>
-                <b>{item.name}:</b>
-                {item.value}
-              </div>
-            ))}
-          </div>
-        </Panel>
+        {detail.response.headers[0] && (
+          <Panel header="Response Headers" key="2">
+            <div className={styles.list}>
+              {detail.response.headers.map((item: any) => (
+                <div className={styles.listItem}>
+                  <b>{item.name}:</b>
+                  {item.value}
+                </div>
+              ))}
+            </div>
+          </Panel>
+        )}
         <Panel header="Request Headers" key="3">
           <div className={styles.list}>
             {detail.request.headers.map((item: any) => (
